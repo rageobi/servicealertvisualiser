999ba6f91d43190ec5966ba3c2a0dd66629874c6
Abinanthan raghunathan <abinanthan007@gmail.com>
2020-04-17 04:15:35 +0530

    Finished QT

---
+++ mainwindow.cpp
@@ -0,0 +1,188 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QDebug>
+
+MainWindow *handle;
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{       ui->setupUi(this);
+        this->timeData="00:00:00";
+        this->setWindowTitle("EE513 Assignment 2");
+        setGraph();
+/*
+    QStringListModel * model = new QStringListModel;
+
+        for (int x = 0; x < 1000; x++)
+        {
+            model->insertRow(model->rowCount());
+            model->setData(model->index(model->rowCount() - 1, 0), QString("hello%1").arg(x), Qt::DisplayRole);
+        }
+
+
+
+        QSortFilterProxyModel * proxy = new QSortFilterProxyModel;
+        proxy->setSourceModel(model);
+
+        this->ui->comboBox->setModel(proxy);
+        this->ui->comboBox->setEditable(true);
+        this->ui->comboBox->setCompleter(0);
+
+        // When the edit text changes, use it to filter the proxy model.
+        connect(this->ui->comboBox, SIGNAL(editTextChanged(QString)), proxy, SLOT(setFilterWildcard(QString)));*/
+    //this->ui->customPlot->replot(); this->ui->comboBox->setEditable(true);
+        ui->topicEdit->setText(TOPIC);
+        QObject::connect(this, SIGNAL(messageSignal(QString)),
+                         this, SLOT(on_MQTTmessage(QString)));
+        ::handle = this;
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::setGraph(){
+
+    this->ui->customPlot->setInteraction(QCP::iRangeZoom,true);
+    this->ui->customPlot->setInteraction(QCP::iRangeDrag,true);
+    //this->ui->customPlot->axisRect()->setRangeDrag(Qt::Horizontal);
+    this->ui->customPlot->axisRect()->setRangeDrag(Qt::Vertical);
+     this->ui->customPlot->axisRect()->setRangeZoom(Qt::Horizontal);
+    this->ui->customPlot->addGraph()->setName("IsLine");
+
+    if(this->dataSelected=="Tempreature"){
+        this->yLabel="Tempreature (celsius)";
+    }
+    else if(this->dataSelected=="Pitch"){
+        this->yLabel="Pitch (degrees)";
+    }
+    else{
+        this->yLabel="Roll (degrees)";
+    }
+    qDebug()<<"YLabel is"<<yLabel;
+    this->ui->customPlot->yAxis->setLabel(yLabel);
+
+    QSharedPointer<QCPAxisTickerTime> timeTicker(new QCPAxisTickerTime);
+    timeTicker->setTimeFormat("%h:%m:%s");
+    this->ui->customPlot->xAxis->setTicker(timeTicker);
+    this->ui->customPlot->yAxis->setRange(-5,5);
+    ui->customPlot->replot();
+
+}
+void MainWindow::update(){
+    // For more help on real-time plots, see: http://www.qcustomplot.com/index.php/demos/realtimedatademo
+
+    QTime time =QTime::fromString(timeData,"hh:mm:ss");
+    if(this->flag ==0){
+        double tSeconds= time.second()+time.minute()*60+time.hour()*3600;
+        this->ui->customPlot->xAxis->setRange(tSeconds-500,tSeconds+3000);
+        this->ui->customPlot->yAxis->setRange(yData*3/5,yData*1.5);this->flag=1;
+    }
+    qDebug()<<"Debug time is"<<time;
+    double xData = time.second()+time.minute()*60+time.hour()*3600; // time elapsed since start of demo, in seconds
+    ui->customPlot->graph(0)->addData(xData,yData);
+    ui->customPlot->graph(0)->rescaleKeyAxis(true);
+    ui->customPlot->replot();
+    QString text = QString("The %2 value is %1").arg(QString::number(this->yData),this->dataSelected);
+    ui->outputEdit->setText(text);
+}
+
+
+void MainWindow::on_connectButton_clicked()
+{
+    MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
+    int rc;this->yData = 0.00;
+    MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    opts.keepAliveInterval = 20;
+    opts.cleansession = 1;
+    opts.username = AUTHMETHOD;
+    opts.password = AUTHTOKEN;
+
+    if (MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered)==0){
+        ui->outputText->appendPlainText(QString("Callbacks set correctly"));
+    }
+    if ((rc = MQTTClient_connect(client, &opts)) != MQTTCLIENT_SUCCESS) {
+        ui->outputText->appendPlainText(QString("Failed to connect, return code %1").arg(rc));
+    }
+    ui->outputText->appendPlainText(QString("Subscribing to topic " TOPIC " for client " CLIENTID));
+    int x = MQTTClient_subscribe(client, ui->topicEdit->text().toLatin1().data(), QOS);
+    ui->outputText->appendPlainText(QString("Result of subscribe is %1 (0=success)").arg(x));
+}
+
+void delivered(void *context, MQTTClient_deliveryToken dt) {
+    (void)context;
+    // Please don't modify the Window UI from here
+    qDebug() << "Message delivery confirmed";
+    handle->deliveredtoken = dt;
+}
+
+/* This is a callback function and is essentially another thread. Do not modify the
+ * main window UI from here as it will cause problems. Please see the Slot method that
+ * is directly below this function. To ensure that this method is thread safe I had to
+ * get it to emit a signal which is received by the slot method below */
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
+    (void)context; (void)topicLen;
+    qDebug() << "Message arrived (topic is " << topicName << ")";
+    qDebug() << "Message payload length is " << message->payloadlen;
+    QString payload;
+    payload.sprintf("%s", (char *) message->payload).truncate(message->payloadlen);
+    emit handle->messageSignal(payload);
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+    return 1;
+}
+
+/** This is the slot method. Do all of your message received work here. It is also safe
+ * to call other methods on the object from this point in the code */
+void MainWindow::on_MQTTmessage(QString payload){
+
+    ui->outputText->appendPlainText(payload);
+    ui->outputText->ensureCursorVisible();
+    QJsonDocument json = QJsonDocument::fromJson(payload.toUtf8());
+    QJsonObject obj = json.object();
+    QJsonValue value=obj.value(QString("d"));
+    QJsonObject data = value.toObject();
+
+     this->pitch=data["pitch"].toDouble();
+     this->roll=data["roll"].toDouble();
+     this->temp=data["temp"].toDouble();
+     this->timeData=data["time"].toString();
+
+     qDebug()<<"time"<<timeData;
+     qDebug()<<"pitch"<<pitch;
+     qDebug()<<"roll"<<roll;
+     qDebug()<<"temp"<<temp;
+     if(dataSelected=="Tempreature"){
+        this->yData=this->temp;
+     }
+     else if(dataSelected=="Pitch"){
+       this->yData=this->pitch;
+     }
+     else{
+       this->yData=this->roll;
+}
+     this->update();
+}
+
+void connlost(void *context, char *cause) {
+    (void)context; (void)*cause;
+    // Please don't modify the Window UI from here
+    qDebug() << "Connection Lost" << endl;
+}
+
+void MainWindow::on_disconnectButton_clicked()
+{
+    qDebug() << "Disconnecting from the broker" << endl;
+    MQTTClient_disconnect(client, 10000);
+    //MQTTClient_destroy(&client);
+}
+
+void MainWindow::on_comboBox_currentTextChanged(const QString &arg1)
+{
+    qDebug()<<"Combobox val is:"<<arg1;
+    dataSelected=arg1;
+    this->flag=0;
+    this->ui->customPlot->graph(0)->data().data()->clear();
+    this->setGraph();
+}
---
+++ mainwindow.h
@@ -0,0 +1,58 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDateTime>
+#include "MQTTClient.h"
+
+#define ADDRESS     "tcp://192.168.0.177:1883"
+#define CLIENTID    "QTClient"
+#define AUTHMETHOD  "rageobi"
+#define AUTHTOKEN   "llll"
+#define TOPIC       "ee513/#"
+#define PAYLOAD     "Hello World!"
+#define QOS         1
+#define TIMEOUT     10000L
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+private slots:
+    void on_connectButton_clicked();
+    void on_disconnectButton_clicked();
+    void on_MQTTmessage(QString message);
+
+    void on_comboBox_currentTextChanged(const QString &arg1);
+
+signals:
+    void messageSignal(QString message);
+
+private:
+    Ui::MainWindow *ui;
+    void update();
+    void setGraph();
+    double pitch,roll,temp,yData;
+    int flag=0;
+    QString timeData,dataSelected="Tempreature",yLabel="Tempreature (celsius)";
+    MQTTClient client;
+
+    volatile MQTTClient_deliveryToken deliveredtoken;
+
+    friend void delivered(void *context, MQTTClient_deliveryToken dt);
+    friend int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message);
+    friend void connlost(void *context, char *cause);
+};
+
+void delivered(void *context, MQTTClient_deliveryToken dt);
+int  msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message);
+void connlost(void *context, char *cause);
+
+#endif // MAINWINDOW_H
+
